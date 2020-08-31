1263af70110e2453fec325f4cc6831dac6421657
Harivansh Mathur <mathurharivansh811@gmail.com>
2020-08-31 14:41:49 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,47 @@ import About from '../src/About';
 import Skill from '../src/Skills';
 import MyProjects from '../src/Projects';
 import ContactMe from '../src/Contact';
+import { LoopCircleLoading } from 'react-loadingg';
 
-export default function App() {
-  return(
-    <div>
-      <MyName />
-      <About />
-      <Skill />
-      <MyProjects />
-      <ContactMe />
-    </div>
-  );
+
+
+const Loading =()=>
+  <div className="loading">
+    <div></div>
+    <div></div>
+  </div>  
+
+
+
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+  
+  componentDidMount(){
+    this.isLoading = setTimeout(()=>{this.setState({loading: false})}, 2300);
+  }
+  componentWillUnmount() {
+     clearTimeout(this.isLoading);
+  }
+  
+  timer = () => setTimeout(()=>{
+    this.setState({loading: false})
+  }, 2300);
+
+  render() {
+    const {loading} = this.state;
+    return (
+      loading ? (<LoopCircleLoading color="#e43f5a" size="large"/>)
+      :(<div className="content">
+          <MyName />
+          <About />
+          <Skill />
+          <MyProjects />
+          <ContactMe />
+      </div>)
+    )
+  }
 }
+
+export default App;
